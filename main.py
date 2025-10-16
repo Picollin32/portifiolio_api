43e5afdf49b944456c51203450ebb7ec617a86bd
Felipe <felipe.g@estudantes.ifg.edu.br>
2025-10-16 20:30:06 -0300

    variavel

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from users import user_controller
 from roles import role_controller
 from auth import auth_controller
-from database import engine, Base
+from database import APP_PROFILE, engine, Base
 
 Base.metadata.create_all(bind=engine)
 
@@ -24,6 +24,28 @@ app.add_middleware(
 app.include_router(user_controller.router)
 app.include_router(role_controller.router)
 app.include_router(auth_controller.router)
+    # Configuração para produção
+
+if APP_PROFILE == "PROG":
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "https://programacaoiii-front.onrender.com",
+            "https://programacaoiii-front.onrender.com/",
+        ],
+        allow_credentials=True,
+        allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allow_headers=["*"],
+    )
+else:
+    # Configuração permissiva para desenvolvimento
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=False,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
